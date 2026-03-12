532e4fa83812d38d1e9a3bd8e843dbeb1cbd47c9
kadampravin62x <kadampravin62@gmail.com>
2026-03-12 16:39:36 +0530

    add login funtion

--- tests/saucelab.spec.js
+++ tests/saucelab.spec.js
@@ -3,6 +3,10 @@ import { test, expect } from '@playwright/test';
 test ('test1', async ({page}) => {
 
     await page.goto("https://www.saucedemo.com/")
+    await page.getByPlaceholder("Username").fill('standard_user')
+    await page.getByPlaceholder("Password").fill('secret_sauce')
+    await page.getByRole('button',{name:'Login'}).click()
+
 
 }
 );
